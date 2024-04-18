735c6a9ac2eadbe12556b0ba38b7f423876c8d29
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-18 14:21:40 +0300

    Базовые классы

--- DEPO Computers_DataBase/Company.cs
+++ DEPO Computers_DataBase/Company.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DEPO_Computers_DataBase
 {
-    internal class Company
+    public class Company
     {
+        public string Name { get; set; } = "Компания";
+        public string TIN { get; set; } = "ИНН";//ИНН
+        public string LegalAddress { get; set; } = "Улица Название 0, г. Город ";
+        public string ActualAddress { get; set; }= "Улица Название 0, г.Город ";
+
+        public Company() { }
     }
 }
---
+++ DEPO Computers_DataBase/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPO_Computers_DataBase
+{
+    internal class Employee
+    {
+        public string FirstName="Фамилия";
+        public string LastName="Имя";
+        public string BirthDay ="День Рождения";
+        public string PassportSerial="123456";
+        public string PassportNumber= "1234";
+        public Employee() { }   
+
+    }
+}
